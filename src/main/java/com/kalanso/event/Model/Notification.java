75cedc2530fafa51585c7f5c4e05823d814c5839
RCHOUATT <cheickhamedouattara865@gmail.com>
2024-07-12 15:02:20 +0000

    12/01/2024 15:02

--- src/main/java/com/kalanso/event/Model/Notification.java
+++ src/main/java/com/kalanso/event/Model/Notification.java
@@ -2,25 +2,28 @@ package com.kalanso.event.Model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Data
 @Table(name = "notifications")
+@NoArgsConstructor
 public class Notification {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @ManyToOne
-    @JoinColumn(name = "participant_id")
-    private Participant participant;
+    @JoinColumn(name = "user_id")
+    private Utilisateur utilisateur;
+
     @ManyToOne
     @JoinColumn(name = "evenement_id")
     private Evenement evenement;
+
     private String type;
     private LocalDateTime dateEnvoi;
-    private String moyen;
     private String statutEnvoi;
-    // Getters, setters, constructeurs
 }
