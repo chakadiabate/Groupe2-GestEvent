4e61791030ea7d0558dba919d546e3253d0124ef
chakadiabate <cdiabate801@gmail.com>
2024-07-26 10:25:28 +0000

    26/07/2024 10:25

--- src/main/java/com/kalanso/event/Model/Prestateur.java
+++ src/main/java/com/kalanso/event/Model/Prestateur.java
@@ -18,6 +18,7 @@ public class Prestateur {
     private String nom;
     private String email;
     private Integer tel;
+    private String profile;
 
     @OneToMany(mappedBy = "prestateur")
     private List<Equipement> equipement;
